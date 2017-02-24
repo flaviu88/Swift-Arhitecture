bebf32bf4f65babdd72a5785ff81c8a64aca665a
flaviu88 <flaviu.silaghi@3pillarglobal.com>
2017-02-24 11:44:27 +0200

    create projects for all athitectures

---
+++ MVP/Swift-MVP/Swift-MVP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-MVP
+//
+//  Created by Flaviu Silaghi on 24/02/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
